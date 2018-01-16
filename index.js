ad0923e497932020d19ff96c0eedcd6d3e0d9b92
Veronica Giraldo <veronica9893@gmail.com>
2018-01-16 00:27:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ function appendKitten(name){
 function prependKitten(name) {
   return [name,...kittens]
 }
-function removeLastKitten() {
-  return kitten.slice(0,kittens,-1)
-}
 function removeFirstKitten() {
-  return kitten.slice(1)
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
 }
